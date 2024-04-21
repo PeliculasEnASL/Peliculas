960096fe800f0e311562c679895f8e619d4a1f72
PeliculasEnASL <165607315+PeliculasEnASL@users.noreply.github.com>
2024-04-21 09:52:02 -0700

    Update movies.py

--- movies.py
+++ movies.py
@@ -4,11 +4,25 @@ st.title('Peliculas en ASL')
 st.header("Español")
 outer_col = st.columns([1,1])
 with outer_col[0]:
-    st.markdown("<a href='https://streamable.com/e/gu49vo?' target='_blank'><img width='200' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/download.jpeg'></a>", unsafe_allow_html=True)
+    st.markdown("<a href='https://streamable.com/e/gu49vo?' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/Frozen2.jpeg'></a>", unsafe_allow_html=True)
+    st.markdown("<a href='https://streamable.com/e/rpocia?' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/TheSantaClause.jpeg'></a>", unsafe_allow_html=True)
+    st.markdown("<a href='https://streamable.com/k0o4gg' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/TheSantaClause.jpeg'></a>", unsafe_allow_html=True)
+
+
+with outer_col[1]:
+    st.markdown("<a href='https://streamable.com/e/xbi2nq?' target='_blank'><img width='400' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/AlmaNochebuena.jpeg'></a>", unsafe_allow_html=True)
+    st.markdown("<a href='https://streamable.com/e/7949vm?' target='_blank'><img width='200' src='https://github.com/PeliculasEnASL/Peliculas/main/images/puberty.jpg'></a>", unsafe_allow_html=True)
+ 
+    
+
 
 st.header("English")
 outer_col = st.columns([1,1])
 with outer_col[0]:
-    st.markdown("<a href='https://streamable.com/e/qlc70u?' target='_blank'><img width='200' src='https://raw.githubusercontent.com/celenaaponce/mobile_practice/main/images/download.jpeg'></a>", unsafe_allow_html=True)
+    st.markdown("<a href='https://streamable.com/e/qlc70u?' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/Frozen2.jpeg'></a>", unsafe_allow_html=True)
+
+with outer_col[1]:
+    st.markdown("<a href='https://streamable.com/e/20rxul?' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/TheLittleMermaid.jpeg'></a>", unsafe_allow_html=True)
+
 
     
