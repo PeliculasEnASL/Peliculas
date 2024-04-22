38eaf4816194328702cfe6182843800ae0cb95a2
PeliculasEnASL <165607315+PeliculasEnASL@users.noreply.github.com>
2024-04-21 20:27:44 -0700

    Update movies.py

--- movies.py
+++ movies.py
@@ -1,33 +1,18 @@
 import streamlit as st
-from st_click_detector import click_detector
-def images(size):
-      content= f"""
-         <br>
-         <br>
-         <a href='#' id='Image 1'><img width='{size}%' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/Frozen2.jpeg'></a>
-         <a href='#' id='Image 2'><img width='{size}%' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/TheSantaClause.jpeg'></a>
-         <a href='#' id='Image 3'><img width='{size}%' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/puberty.jpg'></a>
-         <a href='#' id='Image 4'><img width='{size}%' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/AlmaNochebuena.jpeg'></a>
-         <a href='#' id='Image 5'><img width='{size}%' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/christmascarol.jpeg'></a>
 
-         """
-
-      clicked = click_detector(content)
-      return clicked
 st.title('Peliculas en ASL')
 st.header("Español")
-clicked = images(40)
-st.markdown(f"**{clicked} clicked**" if clicked != "" else "**No click**")
-# outer_col = st.columns([1,1])
-# with outer_col[0]:
-#     st.markdown("<a href='https://streamable.com/e/gu49vo?' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/Frozen2.jpeg'></a>", unsafe_allow_html=True)
-#     st.markdown("<a href='https://streamable.com/e/rpocia?' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/TheSantaClause.jpeg'></a>", unsafe_allow_html=True)
-#     st.markdown("<a href='https://streamable.com/k0o4gg' target='_blank'><img width='400' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/puberty.jpg'></a>", unsafe_allow_html=True)
+
+outer_col = st.columns([1,1])
+with outer_col[0]:
+    st.markdown("<a href='https://streamable.com/e/gu49vo?' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/Frozen2.jpeg'></a>", unsafe_allow_html=True)
+    st.markdown("<a href='https://streamable.com/e/rpocia?' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/TheSantaClause.jpeg'></a>", unsafe_allow_html=True)
+    st.markdown("<a href='https://streamable.com/k0o4gg' target='_blank'><img width='500' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/puberty.jpg'></a>", unsafe_allow_html=True)
 
 
-# with outer_col[1]:
-#     st.markdown("<a href='https://streamable.com/e/xbi2nq?' target='_blank'><img width='400' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/AlmaNochebuena.jpeg'></a>", unsafe_allow_html=True)
-#     st.markdown("<a href='https://streamable.com/e/7949vm?' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/christmascarol.jpeg'></a>", unsafe_allow_html=True)
+with outer_col[1]:
+    st.markdown("<a href='https://streamable.com/e/xbi2nq?' target='_blank'><img width='500' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/AlmaNochebuena.jpeg'></a>", unsafe_allow_html=True)
+    st.markdown("<a href='https://streamable.com/e/7949vm?' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/christmascarol.jpeg'></a>", unsafe_allow_html=True)
  
     
 
