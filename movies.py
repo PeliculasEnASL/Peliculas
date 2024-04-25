59154cf9b98de786d2c72869681e6cdb1bd42f5d
PeliculasEnASL <165607315+PeliculasEnASL@users.noreply.github.com>
2024-04-25 13:15:17 -0700

    Update movies.py

--- movies.py
+++ movies.py
@@ -8,7 +8,7 @@ with outer_col[0]:
     st.markdown("<a href='https://streamable.com/e/gu49vo?' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/Frozen2.jpeg'></a>", unsafe_allow_html=True)
     st.markdown("<a href='https://streamable.com/e/rpocia?' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/TheSantaClause.jpeg'></a>", unsafe_allow_html=True)
     st.markdown("<a href='https://streamable.com/k0o4gg' target='_blank'><img width='400' class = 'center' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/puberty.jpg'></a>", unsafe_allow_html=True)
-    st.markdown("<a href='https://streamable.com/qi4hv1' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/InsideOut.jpg'></a>", unsafe_allow_html=True)
+    st.markdown("<a href='https://streamable.com/qi4hv1' target='_blank'><img width='200' class = 'center' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/InsideOut.jpg'></a>", unsafe_allow_html=True)
 
 with outer_col[1]:
     st.markdown("<a href='https://streamable.com/e/xbi2nq?' target='_blank'><img width='400' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/AlmaNochebuena.jpeg'></a>", unsafe_allow_html=True)
