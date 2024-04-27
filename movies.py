66fc2de17476617c5d37c6de9026ad5512875306
PeliculasEnASL <165607315+PeliculasEnASL@users.noreply.github.com>
2024-04-27 10:01:51 -0700

    Update movies.py

--- movies.py
+++ movies.py
@@ -9,14 +9,14 @@ with outer_col[0]:
     st.markdown("<p align='center'><a href='https://streamable.com/e/rpocia' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/TheSantaClause.jpeg'></a></p>", unsafe_allow_html=True)
     st.markdown("<a href='https://streamable.com/k0o4gg' target='_blank'><img width='400' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/puberty.jpg'></a>", unsafe_allow_html=True)
     st.markdown("<p align='center'><a href='https://streamable.com/qi4hv1' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/InsideOut.jpg'></a></p>", unsafe_allow_html=True)
-    st.markdown("<p align='center'><a href='https://streamable.com/mrx7rr' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/Lilo.jpg'></a></p>", unsafe_allow_html=True)
+    st.markdown("<p align='center'><a href='https://streamable.com/mrx7rr' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/Lilo.jpeg'></a></p>", unsafe_allow_html=True)
 with outer_col[1]:
     st.markdown("<a href='https://streamable.com/e/xbi2nq' target='_blank'><img width='400' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/AlmaNochebuena.jpeg'></a>", unsafe_allow_html=True)
     st.text("Alma's Way Nochebuena")
     st.markdown("<p align='center'><a href='https://streamable.com/e/7949vm' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/christmascarol.jpeg'></a></p>", unsafe_allow_html=True)
     st.markdown("<p align='center'><a href='https://streamable.com/e/0rfzvf' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/TheLittleMermaid.jpeg'></a></p>", unsafe_allow_html=True)
     st.markdown("<p align='center'><a href='https://streamable.com/57m202' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/Toy_Story.jpg'></a></p>", unsafe_allow_html=True)
-    st.markdown("<p align='center'><a href='https://streamable.com/w9vu7s' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/Moana.jpg'></a></p>", unsafe_allow_html=True)
+    st.markdown("<p align='center'><a href='https://streamable.com/w9vu7s' target='_blank'><img width='200' src='https://raw.githubusercontent.com/PeliculasEnASL/Peliculas/main/images/Moana.jpeg'></a></p>", unsafe_allow_html=True)
  
     
 
